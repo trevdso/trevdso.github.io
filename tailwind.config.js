19abd585a3c059ccb63190717a797cffb78d5afc
trevdso <dsouzatrevor97@gmail.com>
2021-09-05 13:38:25 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
         '3xl': '1600px',
-        '4xl': '1920px'
+        '4xl': '1920px',
       },
     },
   },
@@ -13,4 +13,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
