5e7a7aae1ae17bc525c8a24d70bd462837d08e20
trevdso <dsouzatrevor97@gmail.com>
2021-09-05 14:42:35 -0400

    Fixed responsiveness

--- src/components/WelcomeCanvas/WelcomeCanvas.jsx
+++ src/components/WelcomeCanvas/WelcomeCanvas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import pJson from '../../../package.json';
+import pJson from '../../../package.json';
 import './WelcomeCanvas.css';
 
 function WelcomeCanvas() {
@@ -7,12 +7,10 @@ function WelcomeCanvas() {
   const message =
     'This page is currently under construction and is looking at an estimated date of October 20th to go live! Cheers and thank you for being patient :)';
   return (
-    <div className="container">
-      <div className="holder">
-        <div className="header">{header}</div>
-        <div className="message">
-          {message} {/*<div className="font-bold">ver{pJson.version}</div>*/}
-        </div>
+    <div className="holder">
+      <div className="header">{header}</div>
+      <div className="message">
+        {message} <div className="font-bold">v{pJson.version}</div>
       </div>
     </div>
   );
